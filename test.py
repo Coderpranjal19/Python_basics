bfaaabdf07fc80bd8c8daa5ef5e8f9043a2e8702
Pranjal Patel <152408896+Coderpranjal19@users.noreply.github.com>
2024-06-13 19:34:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-class Customer:
-    def __init__(self,name,dob):
-        self.name = name
-        self.dob = dob
-
-    def __str__(self):
-        return f"{self.name},{self.dob}"
-        
-    def m1(self):
-    	print(self.name,'\t',self.dob)
-    
-    
-        
-c = Customer("pranjal",191199)
-c.m1()
-print(c)
+print "wassup buddy"
